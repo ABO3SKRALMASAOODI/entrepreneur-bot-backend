5058bf24d52b8687e611e0df8899cdb12f7d6ba4
MUSLIM <muslimtariq0@gmail.com>
2025-06-28 16:25:26 +0400

    Your commit message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from routes.auth import auth_bp
 from routes.chat import chat_bp
 from routes.verify_email import verify_bp
 from routes.paddle_webhook import paddle_bp
-from routes.paddle_checkout import paddle_checkout_bp  # ✅ Must be here
+
 from models import init_db
 import os
 from dotenv import load_dotenv
@@ -28,8 +28,7 @@ def create_app():
     app.register_blueprint(auth_bp, url_prefix='/auth')
     app.register_blueprint(chat_bp, url_prefix='/chat')
     app.register_blueprint(verify_bp, url_prefix='/verify')
-   # app.register_blueprint(paddle_bp, url_prefix='/paddle')
-    app.register_blueprint(paddle_checkout_bp, url_prefix='/paddle')
+  
     app.register_blueprint(paypal_bp, url_prefix='/paypal')
 
  
